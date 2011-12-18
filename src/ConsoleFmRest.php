9e14aba528ed60f28ec644fb9e0b41795731acf2
Kevin Stanton <stantonk@gmail.com>
2011-12-18 00:31:30 -0600

    started creating class based API

--- src/ConsoleFmRest.php
+++ src/ConsoleFmRest.php
@@ -1,5 +1,64 @@
 <?php
 
+require_once('./config.php');
+
+class ConsoleFmRest
+{
+	const BASE = 'http://console.fm/api';
+
+	public $version;
+
+	private $actions = array(
+		'artists' => '/artists/',
+		'genres' => '/genres/',
+		'users' => 'users'
+	);
+
+	/**
+	 *
+	 *
+	 */
+	public function __construct($apiVersion='v1') {
+		$this->version = $apiVersion;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	//TODO: Should be: private function genUri($desiredAction) {
+	public function genUri($desiredAction) {
+		$url = self::BASE . '/' . $this->version;
+
+		if (array_key_exists($desiredAction, $this->actions)) {
+			$url .= $this->actions[$desiredAction];
+		} else {
+			throw new Exception("Exception: Unknown desiredAction: $desiredAction");
+		}
+
+		return $url;
+	}
+
+
+/*
+	public function getGenres() {
+	}
+*/
+
+}
+
+$myConsoleFm = new ConsoleFmRest();
+
+try {
+	echo $myConsoleFm->genUri('genres') . "\n";
+	echo $myConsoleFm->genUri('ducks') . "\n";
+} catch (Exception $e) {
+	echo "Whoops\n";
+}
+
+
+
+/*
 $base = 'http://console.fm/api/v1';
 $action = array(
 	'genres' => '/genres/',
@@ -8,14 +67,15 @@ $action = array(
 $query_string = "";
 
 $params = array(
-	'api_key' => 'YOUR_API_KEY_HERE'
+	'api_key' => $API_KEY
 );
 
 foreach ($params as $key => $value) {
     $query_string .= "$key=" . urlencode($value) . "&";
 }
 
-$url .= $base . $action['genres'] . '?' . $query_string;
-//$url .= $base . $action['users'] . '?' . $query_string;
+$url = $base . $action['genres'] . '?' . $query_string;
+//$url = $base . $action['users'] . '?' . $query_string;
 $output = file_get_contents($url);
 echo $output;
+*/
